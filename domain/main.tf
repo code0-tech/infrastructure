95bbed96e1490816372d288d523ef578d4944749
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-02-09 20:09:28 +0100

    Create initial infrastructure setup

---
+++ domain/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.12.0"
+    }
+  }
+}
+
+data "cloudflare_zone" "main_domain" {
+  account_id = var.cloudflare_account_id
+  name       = "code0.tech"
+}
