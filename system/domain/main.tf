ff3f11e5e367c93ea57524e4c8beca7d7f47d752
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-02-24 01:58:01 +0100

    Reorganize GitLab project structure

--- system/domain/main.tf
+++ system/domain/main.tf
@@ -29,7 +29,7 @@ module "docs_pages" {
 
   cloudflare_domain_name = "docs"
   cloudflare_zone_id = data.cloudflare_zone.main_domain.id
-  gitlab_project_path = "code0-tech/telescopium"
+  gitlab_project_path = "code0-tech/development/telescopium"
   gitlab_unique_pages_url = "docs-code0-tech-c91f18c0d2259c041bf05138b194e6bb082059fe38eff2e.gitlab.io"
 }
 
