3592a3083c2b32709fd81ce87542e756785c9812
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-07-13 20:54:00 +0200

    Setup plane module

---
+++ modules/docker/plane/images.tf
@@ -0,0 +1,31 @@
+resource "docker_image" "plane_frontend" {
+  name = "makeplane/plane-frontend:v0.22-dev"
+}
+
+resource "docker_image" "plane_space" {
+  name = "makeplane/plane-space:v0.22-dev"
+}
+
+resource "docker_image" "plane_admin" {
+  name = "makeplane/plane-admin:v0.22-dev"
+}
+
+resource "docker_image" "plane_backend" {
+  name = "makeplane/plane-backend:v0.22-dev"
+}
+
+resource "docker_image" "plane_proxy" {
+  name = "makeplane/plane-proxy:v0.22-dev"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:15.5-alpine"
+}
+
+resource "docker_image" "valkey" {
+  name = "valkey/valkey:7.2.5-alpine"
+}
+
+resource "docker_image" "minio" {
+  name = "minio/minio:RELEASE.2024-07-10T18-41-49Z"
+}
---
+++ modules/docker/plane/network.tf
@@ -0,0 +1,3 @@
+resource "docker_network" "plane" {
+  name = "plane"
+}
