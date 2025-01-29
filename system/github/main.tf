9d782c41c96980923df36ac38d652d09603f535c
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-01-29 21:03:03 +0100

    Add webhook for private repositories

--- system/github/main.tf
+++ system/github/main.tf
@@ -19,6 +19,10 @@ data "github_repositories" "public_repositories" {
   query = "org:code0-tech visibility:public"
 }
 
+data "github_repositories" "private_repositories" {
+  query = "org:code0-tech visibility:private"
+}
+
 module "global_labels" {
   source = "../../modules/github/global_labels"
 
@@ -26,12 +30,17 @@ module "global_labels" {
   repository = each.value
 }
 
-data "gitlab_project_variable" "github_discord_webhook_url" {
+data "gitlab_project_variable" "github_public_discord_webhook_url" {
   project = "code0-tech/secret-manager"
-  key     = "GITHUB_DISCORD_WEBHOOK_URL"
+  key     = "GITHUB_PUBLIC_DISCORD_WEBHOOK_URL"
 }
 
-resource "github_repository_webhook" "discord_webhook" {
+data "gitlab_project_variable" "github_private_discord_webhook_url" {
+  project = "code0-tech/secret-manager"
+  key     = "GITHUB_PRIVATE_DISCORD_WEBHOOK_URL"
+}
+
+resource "github_repository_webhook" "public_discord_webhook" {
   for_each = toset(data.github_repositories.public_repositories.names)
 
   repository = each.value
@@ -50,7 +59,31 @@ resource "github_repository_webhook" "discord_webhook" {
 
   configuration {
     //noinspection HILUnresolvedReference
-    url = data.gitlab_project_variable.github_discord_webhook_url.value
+    url = data.gitlab_project_variable.github_public_discord_webhook_url.value
+    content_type = "json"
+  }
+}
+
+resource "github_repository_webhook" "private_discord_webhook" {
+  for_each = toset(data.github_repositories.private_repositories.names)
+
+  repository = each.value
+
+  events = [
+    "discussion",
+    "fork",
+    "issues",
+    "pull_request",
+    "pull_request_review",
+    "push",
+    "release",
+    "star",
+    "watch"
+  ]
+
+  configuration {
+    //noinspection HILUnresolvedReference
+    url = data.gitlab_project_variable.github_private_discord_webhook_url.value
     content_type = "json"
   }
 }
