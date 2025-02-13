e81fafcdcf8c84118265b8e61b616fc7ff2d487b
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-02-13 18:58:41 +0100

    Cloudflare Provider v5 update

--- modules/gitlab/pages_domain/main.tf
+++ modules/gitlab/pages_domain/main.tf
@@ -16,9 +16,10 @@ data "gitlab_project" "this" {
   path_with_namespace = var.gitlab_project_path
 }
 
-resource "cloudflare_record" "gitlab_pages" {
+resource "cloudflare_dns_record" "gitlab_pages" {
   name     = var.cloudflare_domain_name
   type     = "CNAME"
+  ttl      = 1
   zone_id  = var.cloudflare_zone_id
   content  = var.gitlab_unique_pages_url
   proxied  = true
@@ -28,29 +29,34 @@ resource "cloudflare_record" "gitlab_pages" {
 module "certificate" {
   source = "../../cloudflare/certificate"
 
-  hostname = cloudflare_record.gitlab_pages.hostname
+  hostname = cloudflare_dns_record.gitlab_pages.name
 }
 
-data "cloudflare_origin_ca_root_certificate" "cloudflare_root" {
-  algorithm = "rsa"
+data "http" "cloudflare_root" {
+  url = "https://developers.cloudflare.com/ssl/static/origin_ca_rsa_root.pem"
+
+  retry {
+    attempts = 2
+  }
 }
 
 resource "gitlab_pages_domain" "this" {
   project = data.gitlab_project.this.id
-  domain  = cloudflare_record.gitlab_pages.hostname
+  domain  = cloudflare_dns_record.gitlab_pages.name
 
   key         = module.certificate.private_key
   certificate = <<-EOF
     ${module.certificate.certificate}
-    ${data.cloudflare_origin_ca_root_certificate.cloudflare_root.cert_pem}
+    ${data.http.cloudflare_root.response_body}
   EOF
 }
 
 //noinspection HILUnresolvedReference
-resource "cloudflare_record" "gitlab_pages_verification" {
-  name = "_gitlab-pages-verification-code${var.cloudflare_domain_name == "@" ? "" : format(".%s", var.cloudflare_domain_name)}"
+resource "cloudflare_dns_record" "gitlab_pages_verification" {
+  name = "_gitlab-pages-verification-code.${var.cloudflare_domain_name}"
   type = "TXT"
+  ttl     = 1
   zone_id = var.cloudflare_zone_id
   content = gitlab_pages_domain.this.verification_code
-  comment  = "Managed by Terraform | Pages verification for ${cloudflare_record.gitlab_pages.hostname}"
+  comment  = "Managed by Terraform | Pages verification for ${cloudflare_dns_record.gitlab_pages.name}"
 }
